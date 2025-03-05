d5e961f15637ae03d6203e57d2b4135cfadb5884
Maulaera <mrobi021@odu.edu>
2025-03-05 16:50:03 -0500

    final documentation

--- vis/adelphi_vis.py
+++ vis/adelphi_vis.py
@@ -1,3 +1,7 @@
+# Maulahna Robinson
+# Makendra McCrosby
+# CS620
+
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -5,14 +9,14 @@ import matplotlib.pyplot as plt
 file_path = "data/Adelphi University Class of 2024.xlsx"
 df = pd.read_excel(file_path)
 
-print(df.columns)
+# print(df.columns) (THIS WAS TO MAKE SURE THE DATA WAS BEING READ CORRECTLY)
 
 
 x = df['Ethnicity']
 y = df['New First-Year Students']
 
-print(f"Length of ethnin: {len(x)}")
-print(f"Length of percen: {len(y)}")
+# print(f"Length of ethnin: {len(x)}") (THIS WAS FOR DEBUGGING AND TESTING)
+# print(f"Length of percen: {len(y)}") (THIS WAS FOR DEBUGGING AND TESTING)
 
 y = y.astype(str).str.rstrip('%')
 y = pd.to_numeric(y, errors='coerce')
