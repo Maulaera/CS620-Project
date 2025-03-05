6ffd71d1160e4605d08d0228c01451a05c8ffa3d
Maulaera <mrobi021@odu.edu>
2025-03-05 16:34:04 -0500

    updates

--- data/adelphi_vis.py
+++ data/adelphi_vis.py
@@ -2,18 +2,22 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Load the Excel file
-file_path = "data/Adelphi University Class of 2024.xlsx"
+file_path = "data/Adelphi University Class of 2024 (1).xlsx"
 df = pd.read_excel(file_path)
 
 print(df.columns)
 
-# x = df['Ethnicity']
-# y = df['New First-Year Students']
 
-# y = y.astype(str).str.rstrip('%')
+x = df['Ethnicity']
+y = df['New First-Year Students']
+
+print(f"Length of ethnin: {len(x)}")
+print(f"Length of percen: {len(y)}")
+
+#y = y.astype(str).str.rstrip('%')
 #y = pd.to_numeric(y, errors='coerce')
 
-#lt.bar(x,y) # Bar Chart
+#plt.bar(x,y) # Bar Chart
 
 #plt.xlabel('Student Ethnicities')
 #plt.ylabel('Percentage')
