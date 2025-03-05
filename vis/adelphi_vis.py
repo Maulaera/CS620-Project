6737dfe39ca55efcb0409b7fa8b00ec56867902a
Maulaera <mrobi021@odu.edu>
2025-03-05 16:38:03 -0500

    update adelphi vis, organize files

---
+++ vis/adelphi_vis.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Load the Excel file
+file_path = "data/Adelphi University Class of 2024 (2).xlsx"
+df = pd.read_excel(file_path)
+
+print(df.columns)
+
+
+x = df['Ethnicity']
+y = df['New First-Year Students']
+
+print(f"Length of ethnin: {len(x)}")
+print(f"Length of percen: {len(y)}")
+
+y = y.astype(str).str.rstrip('%')
+y = pd.to_numeric(y, errors='coerce')
+
+plt.bar(x,y) # Bar Chart
+
+plt.xlabel('Student Ethnicities')
+plt.ylabel('Percentage')
+plt.title('Adelphi Admissions by Ethnicity')
+
+plt.show()
