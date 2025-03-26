b36df9467f933b6395f1f578d96a9786d2bb2d69
Maulaera <mrobi021@odu.edu>
2025-03-26 15:58:15 -0400

    GW

---
+++ vis/GW.py
@@ -0,0 +1,9 @@
+# Maulahna Robinson
+# Makendra Crosby
+# CS620
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+file_path = "data/George Washington Class of 2025.xlsx"
+df = pd.read_excel(file_path)
